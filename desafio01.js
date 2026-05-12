f5115c3c1a0bd8e49ff6af62d9a10d85b6610d10
Caetano <heirisoncae@gmail.com>
2026-05-11 21:07:34 -0300

    desafio 01

---
+++ desafio01.js
@@ -0,0 +1,19 @@
+let heroi = "Luffy";
+let xpHeroi = 3500;
+if (xpHeroi <= 1000) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " pontos de experiência, classificando com o nível Ferro")
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " pontos de experiência, classificando com o nível Bronze")
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " pontos de experiência, classificando com o nível Prata")
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " os pontos de experiência, classificando com o nível Ouro")
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " pontos de experiência, classificando com o nível Platina")
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " pontos de experiência, classificando com o nível Ascendente")
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " pontos de experiência, classificando com o nível Imortal")
+} else if (xpHeroi >= 10001) {
+    console.log(" O Herói " + heroi + " tem " + xpHeroi + " pontos de experiência, classificando com o nível Radiante")
+}
